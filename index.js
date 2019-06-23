2f50b8b91ec4091e46fe65d3125f7af066d0c821
Lucas Da Silva <lucashbsj@outlook.com>
2019-06-22 22:57:46 -0300

    feat : add Google trends

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
 // Orquestrador
 const readline = require('readline-sync')
-function start(){
+const Parser = require('rss-parser')
+const TREND_URL = 'https://trends.google.com/trends/trendingsearches/daily/rss?geo=BR'
+	
+async function start()
+{
 	const content = {}
 	
-	content.searchTerm = askAndReturnSearchTerm()
+	content.searchTerm = await askAndReturnSearchTerm()
 	content.prefix = askAndReturnPrefix()
 
-	function askAndReturnSearchTerm(){
-		return readline.question("Type a Wikipedia search term: ")
+	async function askAndReturnSearchTerm()
+	{ 
+		const response = readline.question("Type a wikipedia search term or G to fetch google trends") 
+		return (response.toUpperCase() === 'G') ? await askAndReturnTrend() : response
 	}
-	
-	function askAndReturnPrefix(){
+
+	async function askAndReturnTrend() 
+	{
+		console.log("Please wait...")
+		const trends = await getGoogleTrends()
+		const choice = readline.keyInSelect(trends, "Choose you trend: ")
+
+		return trends[choice]
+	}
+
+	async function getGoogleTrends()
+	{
+		const parser = new Parser()
+		const trends = await parser.parseURL(TREND_URL)
+		return trends.items.map(({title}) => title)
+	}
+			
+	function askAndReturnPrefix()
+	{
 		const prefixes = ["Who is", "What is", "The history of"]
 		const selectedPrefixIndex = readline.keyInSelect(prefixes, "Choose one option: ")
 		const selectedPrefixText = prefixes[selectedPrefixIndex]
